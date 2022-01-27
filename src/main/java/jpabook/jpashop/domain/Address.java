edd7a44045088891135d4e3331103054b0dad1f7
nomoreFt <hyunwoo.kim1108@gmail.com>
2022-01-27 22:38:36 +0900

    Add Entities

--- src/main/java/jpabook/jpashop/domain/Address.java
+++ src/main/java/jpabook/jpashop/domain/Address.java
@@ -6,11 +6,24 @@ import lombok.Setter;
 import javax.persistence.Embeddable;
 
 @Embeddable//jpa의 내장타입이란 뜻
-@Getter @Setter
+@Getter
 public class Address {
 
     private String city;
     private String street;
     private String zipcode;
 
+    /*
+    Setter를 제거하고, 생성자에서 값읆 모두 초기화해서 변경 불가능하게 만들고,
+    JPA 스펙상 임베디드타입, 엔티티는 자바 기본 생성자를 protected로 설정해야한다.
+    JPA 구현 라이브러리가 객체를 생성할 때 리플렉션 같은 기술을 사용할 수 있또록 지원해야하기 때문,
+     */
+    protected Address() {
+    }
+
+    public Address(String city, String street, String zipcode) {
+        this.city = city;
+        this.street = street;
+        this.zipcode = zipcode;
+    }
 }
--- src/main/java/jpabook/jpashop/domain/item/Item.java
+++ src/main/java/jpabook/jpashop/domain/item/Item.java
@@ -1,9 +1,12 @@
 package jpabook.jpashop.domain.item;
 
+import jpabook.jpashop.domain.Category;
 import lombok.Getter;
 import lombok.Setter;
 
 import javax.persistence.*;
+import java.util.ArrayList;
+import java.util.List;
 
 @Entity
 @Inheritance(strategy = InheritanceType.TABLE_PER_CLASS)//strategy =  제일 정교 single 다 때려박기
@@ -18,4 +21,7 @@ public abstract class Item {//상속관계 전략을 심어줘야한다. (여긴
     private String name;
     private int price;
     private int stockQuantity;
+
+    @ManyToMany(mappedBy = "items")
+    private List<Category> categories = new ArrayList<>();
 }
