c15301a2b92045e9d75d913f2833c6f89c313485
nomoreFt <hyunwoo.kim1108@gmail.com>
2022-02-09 23:26:51 +0900

    Add

--- src/test/java/jpabook/jpashop/service/ItemServiceTest.java
+++ src/test/java/jpabook/jpashop/service/ItemServiceTest.java
@@ -1,14 +1,11 @@
 package jpabook.jpashop.service;
 
 import jpabook.jpashop.domain.item.Book;
-import jpabook.jpashop.domain.item.Item;
-import jpabook.jpashop.exception.NotEnoughStockException;
-import org.junit.Assert;
+import jpabook.jpashop.controller.exception.NotEnoughStockException;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.annotation.Rollback;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.transaction.annotation.Transactional;
 
